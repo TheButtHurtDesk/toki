17d7ed99d9d0afc84f8407775b916fdd5bfe6441
2liv <vtcool02@gmail.com>
2026-02-23 21:59:20 +0100

    Makefil + start på mainprogram

---
+++ Makefile
@@ -0,0 +1,18 @@
+files := \
+	main.cpp \
+
+# Compiler (g++)
+CCC = g++
+
+# Compiling flags
+CCFLAGS +=  -Wno-deprecated-declarations -Wall -Wextra -pedantic -std=c++1z -Weffc++ -g
+
+LDFLAGS += -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system
+
+all: compile
+
+compile:
+	$(CCC) $(CCFLAGS) -g $(files) -o game $(LDFLAGS)
+
+run:
+	./game
--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
     std::cout << "1, 2, 3, 4, jag är en party myra!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Toki");
+    sf::CircleShape shape(200);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
     return 0;
 }
